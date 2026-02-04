da4e29d031fa62a14f87bd80ec2e2adb023449ef
justnorriel <fabrianorriel@gmail.com>
2026-02-04 22:22:07 +0800

    RoutingAgain

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,51 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App'
-import HomeSection from './pages/HomeSection'
-import About from './pages/About'
-import Skills from './pages/Skills'
-import Projects from './pages/Projects'
-import Reviews from './pages/Reviews'
-import ReviewDetails from './pages/ReviewDetails'
-import Contact from './pages/Contact'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+import App from './App';
+import HomeSection from './pages/HomeSection';
+import About from './pages/About';
+import Skills from './pages/Skills';
+import Projects from './pages/Projects';
+import Reviews from './pages/Reviews';
+import Contact from './pages/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomeSection />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "skills",
+        element: <Skills />,
+      },
+      {
+        path: "projects",
+        element: <Projects />,
+      },
+      {
+        path: "reviews",
+        element: <Reviews />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<HomeSection />} />
-          <Route path="about" element={<About />} />
-          <Route path="skills" element={<Skills />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="reviews" element={<Reviews />} />
-          <Route path="reviews/:id" element={<ReviewDetails />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
